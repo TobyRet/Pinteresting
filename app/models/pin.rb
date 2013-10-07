a1a6c3711cdbd6ef8629da89a54d17e4b004cdf4
Toby Retallick <toby.retallick@gmail.com>
2013-10-07 18:09:27 +0100

    styled pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,15 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
-
+	
+	validates :description, presence: true
+	validates :user_id, presence: true
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment :image, presence: true,
+															 content_type: { content_type: ['image/jpeg', 'image/jpg']},
+															 size: {less_than: 5.megabytes}
+	belongs_to :user
+	
+	def pin_params
+	   params.require(:pin).permit(:description, :image)
+	end
 end
+
