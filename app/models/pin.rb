277c347d4c39e3c238e2271f2bd9d357f9659b7f
Toby Retallick <toby.retallick@gmail.com>
2013-10-27 20:25:55 +0000

    added names to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,15 +1,10 @@
 class Pin < ActiveRecord::Base
 	
-	validates :description, presence: true
-	validates :user_id, presence: true
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	validates_attachment :image, presence: true,
-															 content_type: { content_type: ['image/jpeg', 'image/jpg']},
-															 size: {less_than: 5.megabytes}
 	belongs_to :user
-	
-	def pin_params
-	   params.require(:pin).permit(:description, :image)
-	end
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
 
